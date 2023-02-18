f86f2cf2522aeb1f2a2943d07c4c7499378e39ef
isaac <isaac.slaughter@gmail.com>
2023-02-17 16:54:50 -0800

    Add SEAT/WEAT Replication

---
+++ eats/seat_replication.py
@@ -0,0 +1,149 @@
+import os
+
+import xml.etree.ElementTree as ET
+
+import torch
+import nltk
+import numpy as np
+import pandas as pd
+import json
+
+from scipy.special import comb
+from CLIP import clip
+from tqdm import tqdm
+from nltk.corpus import wordnet
+from pattern.text.en import pluralize
+
+import os
+
+
+
+
+from eats.extract_clip import extract_images, extract_text, load_words_greenwald
+from eats.sc_weat import SCWEAT, WEAT
+from eats.result_saving import save_test_results
+
+
+def test_already_run(test, file_name):
+    if not os.path.exists(file_name):
+        return False
+    previous_results = pd.read_csv(file_name)
+    relevant_results = previous_results[
+        (previous_results['model'] == test['model'])
+        & (previous_results['test_name'] == test['test_name'])
+    ]
+    return len(relevant_results) > 0
+
+
+def get_all_image_paths():
+    all_images = []
+    for root, dir, files in os.walk(os.path.join('data','CFD Version 3.0', 'Images')):
+        for f in files:
+            if f.split('.')[-1] == 'jpg':
+                all_images.append(os.path.join(root, f))
+    return all_images
+
+
+def perform_test(device):
+
+    npermutations = 100000
+
+    results_fp = os.path.join('results', 'data', 'seat_replication.csv')
+    models = clip.available_models()
+    tests = [f for f in os.listdir(os.path.join('data','tests')) if f.split('.')[1] == 'jsonl']
+
+    # models.reverse()
+    total = len(models) * len(tests)
+
+
+    if os.path.exists(results_fp):
+        completed = pd.read_csv(results_fp)
+        completed = [completed['model'].str.contains(a) for a in models]
+        completed = pd.concat(completed, axis=1).any(axis=1).sum()
+    else:
+        completed = 0
+
+    remaining = total - completed
+
+    with tqdm(total=remaining) as pbar:
+        for model_name in models:
+            model, preprocess = clip.load(model_name, device)
+
+            for test_name in tests:
+                try:
+                    stimuli = load_seat(test_name)
+                    test = {'test_name': str(test_name),
+                            'X': stimuli['names'][0],
+                            'Y': stimuli['names'][1],
+                            'A': stimuli['names'][2],
+                            'B': stimuli['names'][3],
+                            'nt': len(stimuli['X']),
+                            'na': len(stimuli['A']),
+                            'model': model_name}
+                except AssertionError as e:
+                    test = {'test_name': str(test_name),
+                            'X': np.NaN,
+                            'Y': np.NaN,
+                            'A': np.NaN,
+                            'B': np.NaN,
+                            'nt': np.NaN,
+                            'na': np.NaN,
+                            'model': model_name}
+
+
+                if not test_already_run(test, results_fp):
+                    if not test['X'] is np.NaN:
+                        np.random.seed(5718980)
+
+
+                        try:
+                            embeddings = {
+                                'X': extract_text(model, preprocess, stimuli['X'], device, model_name),
+                                'Y': extract_text(model, preprocess, stimuli['Y'], device, model_name),
+                                'A': extract_text(model, preprocess, stimuli['A'], device, model_name),
+                                'B': extract_text(model, preprocess, stimuli['B'], device, model_name),
+                            }
+                            test_result = WEAT(embeddings['X'], embeddings['Y'], embeddings['A'], embeddings['B'])
+                            test_result = pd.Series({
+                                'effect_size': test_result.effect_size(),
+                                'p_value': test_result.p(npermutations),
+                                'npermutations': npermutations,
+                            })
+                        except RuntimeError as e:
+                            test_result = pd.Series({}, dtype=str)
+
+                    else:
+                        test_result = pd.Series({}, dtype=str)
+                    save_test_results(pd.concat([pd.Series(test), test_result]), results_fp)
+                    pbar.update()
+
+
+def load_seat(test: str):
+    with open(os.path.join('data', 'tests', test), 'r') as f:
+        stimuli = json.load(f)
+
+    stimuli = {
+        'names': [stimuli['targ1']['category'], stimuli['targ2']['category'],
+                  stimuli['attr1']['category'], stimuli['attr2']['category']],
+        'X': stimuli['targ1']['examples'],
+        'Y': stimuli['targ2']['examples'],
+        'A': stimuli['attr1']['examples'],
+        'B': stimuli['attr2']['examples']
+    }
+    assert(len(stimuli['X']) == len(stimuli['Y']))
+    return stimuli
+
+
+if __name__ == '__main__':
+    should_continue=True
+    while should_continue:
+        if os.path.exists(os.path.join('results','data','seat_replication.csv')):
+            num_results_so_far = len(pd.read_csv(os.path.join('results','data','seat_replication.csv')))
+        else:
+            num_results_so_far = 0
+        try:
+            perform_test('cuda' if torch.cuda.is_available() else 'cpu')
+        except:
+            perform_test('cpu')
+        if num_results_so_far == len(pd.read_csv(os.path.join('results','data','seat_replication.csv'))):
+            should_continue = False
---
+++ eats/weat_replication.py
@@ -0,0 +1,140 @@
+import os
+
+import xml.etree.ElementTree as ET
+
+import torch
+import nltk
+import numpy as np
+import pandas as pd
+
+from scipy.special import comb
+from CLIP import clip
+from tqdm import tqdm
+from nltk.corpus import wordnet
+from pattern.text.en import pluralize
+
+import os
+import openai
+openai.organization = "org-3TyW8U8lCQYmJdyiRSLtSOWm"
+openai.api_key_path = os.getenv("OPENAI_API_KEY")
+openai.Model.list()
+
+
+from eats.extract_clip import extract_images, extract_text, load_words_greenwald
+from eats.sc_weat import SCWEAT, WEAT
+from eats.result_saving import save_test_results
+
+
+def test_already_run(test, file_name):
+    if not os.path.exists(file_name):
+        return False
+    previous_results = pd.read_csv(file_name)
+    relevant_results = previous_results[
+        (previous_results['model'] == test['model'])
+        & (previous_results['weat_num'] == test['weat_num'])
+    ]
+    return len(relevant_results) > 0
+
+
+def get_all_image_paths():
+    all_images = []
+    for root, dir, files in os.walk(os.path.join('data','CFD Version 3.0', 'Images')):
+        for f in files:
+            if f.split('.')[-1] == 'jpg':
+                all_images.append(os.path.join(root, f))
+    return all_images
+
+
+def perform_test(device):
+
+    npermutations = 100000
+
+    results_fp = os.path.join('results', 'data', 'weat_replication.csv')
+    models = clip.available_models()
+
+    models += [
+
+    ]
+
+    # models.reverse()
+    total = len(models) * 10
+
+
+    if os.path.exists(results_fp):
+        completed = pd.read_csv(results_fp)
+        completed = [completed['model'].str.contains(a) for a in models]
+        completed = pd.concat(completed, axis=1).any(axis=1).sum()
+    else:
+        completed = 0
+
+    remaining = total - completed
+
+    with tqdm(total=remaining) as pbar:
+        for model_name in models:
+            model, preprocess = clip.load(model_name, device)
+
+            for weat_num in range(1, 11):
+                stimuli = load_weat(weat_num)
+                test = {'weat_num':str(weat_num),
+                        'X': stimuli['names'][0],
+                        'Y': stimuli['names'][1],
+                        'A': stimuli['names'][2],
+                        'B': stimuli['names'][3],
+                        'nt': len(stimuli['X']),
+                        'na': len(stimuli['A']),
+                        'model': model_name}
+
+                if not test_already_run(test, results_fp):
+                    np.random.seed(5718980)
+
+
+                    embeddings = {
+                        'X': extract_text(model, preprocess, stimuli['X'], device, model_name),
+                        'Y': extract_text(model, preprocess, stimuli['Y'], device, model_name),
+                        'A': extract_text(model, preprocess, stimuli['A'], device, model_name),
+                        'B': extract_text(model, preprocess, stimuli['B'], device, model_name),
+                    }
+
+
+                    test_result = WEAT(embeddings['X'], embeddings['Y'], embeddings['A'], embeddings['B'])
+                    test_result = pd.Series({
+                        'effect_size':test_result.effect_size(),
+                        'p_value': test_result.p(npermutations),
+                        'npermutations':npermutations,
+                    })
+
+                    save_test_results(pd.concat([pd.Series(test), test_result]), results_fp)
+                    pbar.update()
+
+
+def load_weat(weat_num: int):
+    with open(os.path.join('data','weat_words.txt'), 'r') as f:
+        lines = f.readlines()
+    l_start = [i for i, l in enumerate(lines) if l == f'WEAT{weat_num}\n'][0]
+    lines = [l.replace('\n','') for l in lines[l_start+1:l_start+5]]
+
+    stimuli = {
+        'names': [l.split(':')[0] for l in lines],
+        'X': lines[0].split(':')[1].split(','),
+        'Y': lines[1].split(':')[1].split(','),
+        'A': lines[2].split(':')[1].split(','),
+        'B': lines[3].split(':')[1].split(',')
+    }
+    assert(len(stimuli['X']) == len(stimuli['Y']))
+    assert(len(stimuli['A']) == len(stimuli['B']))
+    return stimuli
+
+
+if __name__ == '__main__':
+    should_continue=True
+    while should_continue:
+        if os.path.exists(os.path.join('results','data','weat_replication.csv')):
+            num_results_so_far = len(pd.read_csv(os.path.join('results','data','weat_replication.csv')))
+        else:
+            num_results_so_far = 0
+        try:
+            perform_test('cuda' if torch.cuda.is_available() else 'cpu')
+        except:
+            perform_test('cpu')
+        if num_results_so_far == len(pd.read_csv(os.path.join('results','data','weat_replication.csv'))):
+            should_continue = False
